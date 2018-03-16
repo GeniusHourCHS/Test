2a4a3a862bfd846d16225e5724de729553e25b21
anthony-zhou <37421108+anthony-zhou@users.noreply.github.com>
2018-03-16 11:56:56 -0500

    Working on combining calculator

--- Chatbot/src/Main.java
+++ Chatbot/src/Main.java
@@ -2,17 +2,31 @@ import java.io.*; import java.util.*; import static java.lang.System.*;
 public class Main { 
 	public static void main(String[] args) { 
 		Scanner big = new Scanner(System.in); 
+		String[] nums = {"0","1","2","3","4","5","6","7","8","9"};
+		final List<String> numbers = Arrays.asList(nums);
+		
+		/*
+		 * Introduction and greeting
+		 */
 		out.println("Hello, I am chatbot."); 
 		out.println("I can: add numbers and tell jokes. What's your name?"); 
 		String name = big.next(); 
 		out.println("Great, " + name + ". Now ask me anything."); 
 		big.nextLine();
+		
+		/*
+		 * processing the question
+		 */
 		String question = big.nextLine(); 
 		do { 
+			//String[] q = question.split(" ");
 			
 			Scanner line = new Scanner(question);
 			String s = line.next().toLowerCase();
 			
+			if(numbers.contains(s.substring(0,1))) {
+				//Calculator.
+			}
 			
 			if(s.equals("what")) {
 				out.println("what?");
@@ -23,6 +37,7 @@ public class Main {
 			question = big.nextLine(); 
 			
 		} 
-		while (!question.equals("stop")); 
+		while (!question.equals("bye")); 
+		out.println("okay, bye :'(");
 	} 
 }
