b63c61089fd0b2bff0ae5cebe1f611fc75380c94
SaakethC <saakethrchenna@gmail.com>
2018-03-16 11:19:37 -0500

    Outline of Calculator class

--- Chatbot/src/Calculator.java
+++ Chatbot/src/Calculator.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
 
 public class Calculator {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		double num1 = 0 ;
+		double num2 = 0 ;
+		char operator;
+		double answer = 0.0;
+		
+		Scanner scanObject = new Scanner(System.in);
+		num1 = scanObject.nextDouble();
+		num2 = scanObject.nextDouble();
+		operator = scanObject.next().charAt(0);
+		
+		switch (operator) {
+			
+			case '+': answer = num1 + num2;
+			break;
+			case '-': answer = num1 - num2;
+			break;
+			case '/': answer = num1/num2;
+			break;
+			case '*': answer = num1 * num2;
+			break;
+	
+		}
+		
+		System.out.println(num1+" "+operator+" "+num2+" = "+answer);
+		
 	}
 
 }
