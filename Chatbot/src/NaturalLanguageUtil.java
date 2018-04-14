3b35bfb35eaae3a00008e9e8a62b130a3b73fbe2
anthony-zhou <anthony.zhou03@gmail.com>
2018-04-14 09:47:49 -0500

    Added question methods (w/ javadocs) to NaturalLanguageUtil.java.

--- Chatbot/src/NaturalLanguageUtil.java
+++ Chatbot/src/NaturalLanguageUtil.java
@@ -13,13 +13,15 @@ public class NaturalLanguageUtil {
 		String output = replaceArticles(question.toLowerCase());
 		output = apostropheProcess(output);
 		return output;
+
 	}
 	private static String replaceArticles(String question) {
 		String output = question.replaceAll("the |an |a ", "");
 		return output;
 	}
 
-	/*
+	/**
+	 * processes phrases with apostrophes
 	* @param question is the question inputted by the user.
 	* @return is the question with apostrophes converted to "is"
 	* e.g. "What's" -> "What is "
@@ -38,8 +40,40 @@ public class NaturalLanguageUtil {
 		}
 		return output;
 	}
+
+	/**
+	 * any Question method responds to a question with a question word.
+	 * If the program doesn't know how to respond, it searches the web
+	 * @param question is the question asked by the user,
+	 *                 in format "[question word] [verb] [rest of question]?"
+	 *                 e.g. "who are you?" "what can you do?"
+	 * @return is the computer-generated response
+	 */
+	private static String who(String question) {
+		return question;
+	}
+
+	private static String what(String question) {
+		if(question.equals("what can you do?")) {
+			return "I can tell jokes, do math or flip a coin.";
+		}
+		return question;
+	}
+
+	private static String when(String question) {
+		return question;
+	}
+
 	private static String where(String question) {
 		return question;
 	}
+	private static String why(String question) {
+		return question;
+	}
+	private static String how(String question) {
+		return question;
+	}
+
+
 	
 }
