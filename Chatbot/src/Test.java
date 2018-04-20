057e85366dd68c14ff50aa315203a62c2f4e8278
SaakethC <saakethrchenna@gmail.com>
2018-04-19 19:31:03 -0500

    still trying to fix parser

--- Chatbot/src/Test.java
+++ Chatbot/src/Test.java
@@ -11,17 +11,23 @@ import java.util.Random;
 public class Test {
 	
 	public static void main(String[] args) throws IOException  { 
-
+		
 		String encoding = "UTF-8";
 		
 			String searchText = "who is the president";
-			Document google = Jsoup.connect("https://www.google.com/search?rlz="+ URLEncoder.encode(searchText, encoding)).userAgent("Mozilla/5.0").get();
+			Document google = Jsoup.connect("https://www.google.com/search?q="+ URLEncoder.encode(searchText, encoding)).userAgent("Mozilla/5.0").get();
 			String title = google.title();
 			System.out.println(title);
 			
-			Element link = google.select("div class answer").first();     
-			System.out.println("Text: " + link.text());
+			//Elements test = google.getElementsByClass("CLASS");
 			
+			//System.out.println(test);
+			
+			Elements temp = google.select("div.Z0LcW");
+	 	
+			System.out.println(temp.text());
+	        
+	         
 			//title1 = google.select("div[class=Z0LcW]").text();
 	
 		
@@ -29,6 +35,8 @@ public class Test {
 		        //System.out.println(String.format(msg, vals));
 	}
 }
+
+
 	
 
 	
